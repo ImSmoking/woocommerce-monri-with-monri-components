3f9afeb8cb29d517e36c9682440d1a628d1ed265
Marko <marko@flipkod.com>
2020-10-02 09:20:01 +0200

    UPDATE; Direct payment method updated,now it is using 'monri componets' js plugin.

--- pikpay.php
+++ pikpay.php
@@ -41,3 +41,15 @@ function pikpay_action_links( $links ) {
 	// Merge our new link with the default ones
 	return array_merge( $plugin_links, $links );
 }
+
+
+// A way to stop the form checkout submit event via JQuery could not be found.
+// To simulate this a fake error needs to be thrown so that the generate monri toke can be added to the 'monri-token' form field.
+// Once the token is added checkout process will be triggered via JQuery.
+add_action('woocommerce_after_checkout_validation', 'fake_checkout_form_error');
+function fake_checkout_form_error($posted) {
+
+    if ($_POST['monri-token'] == 'not-set') {
+        wc_add_notice( __( "set_monri_token_notice", 'fake_error' ), 'error');
+    }
+}
